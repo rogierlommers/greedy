28049685e929b858dee12e2134d58448faf26570
Rogier Lommers <rogier@lommers.org>
2015-05-05 14:35:25 +0200

    trying failed builds

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+echo "---------------------------------------------------------------------------------------------------"
 echo "start building version: ${BUILD_NUMBER}"
 rm -rf ./target
 mkdir -p ./target
 
-CGO_ENABLED=0 go build -a -installsuffix cgo -ldflags '-s' -o ./target/go-read go-read.go
+CGO_ENABLED=0 go build -a -installsuffix cgo -ldflags '-s' -o ./target/go-read go-read.goo
 echo "build output code: $?"
 
 cp -r ./static ./target/static
 echo "static copying output code: $?"
+
+echo "---------------------------------------------------------------------------------------------------"
