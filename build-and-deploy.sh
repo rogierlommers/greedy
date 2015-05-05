e7beb070e046c1efd585b8152d272031521c179a
Rogier Lommers <rogier@lommers.org>
2015-05-05 14:12:14 +0200

    added build script

--- build-and-deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "starting build-and-deploy"
-
-mkdir -p ./target
--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-CGO_ENABLED=0 go build -a -installsuffix cgo -ldflags '-s' go-read.go
+rm -rf ./target
+mkdir -p ./target
+
+CGO_ENABLED=0 go build -a -installsuffix cgo -ldflags '-s' -o ./target/go-read go-read.go
+cp -r ./static ./target/static
