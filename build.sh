9d3694e2b318d9dc7a56677835b29ae6041a8703
Rogier Lommers <rogier@lommers.org>
2015-05-05 14:34:23 +0200

    update build script

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+echo "start building version: ${BUILD_NUMBER}"
 rm -rf ./target
 mkdir -p ./target
 
 CGO_ENABLED=0 go build -a -installsuffix cgo -ldflags '-s' -o ./target/go-read go-read.go
+echo "build output code: $?"
+
 cp -r ./static ./target/static
+echo "static copying output code: $?"
