31645e996f80138e3aa8ddcc4208163c761c6948
Rogier Lommers <rogier@lommers.org>
2015-10-09 22:36:50 +0200

    merged rendering package

--- internal/render/render.go
+++ internal/render/render.go
@@ -4,8 +4,22 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/GeertJohan/go.rice"
+
 	"html/template"
 )
+
+var staticBox *rice.Box
+
+func CreateStaticBox() {
+
+	// create rice.box with static files
+	staticBox = rice.MustFindBox("../../static")
+
+	// css files should be exposed through /css endpoint
+	cssFileServer := http.StripPrefix("/css/", http.FileServer(staticBox.HTTPBox()))
+	http.Handle("/css/", cssFileServer)
+}
 
 func DisplayPage(w http.ResponseWriter, r *http.Request, dynamicData interface{}) {
 	templateString, err := staticBox.String("index.tmpl")
